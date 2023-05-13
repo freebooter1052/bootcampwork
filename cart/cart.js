fe0a2c472cdb8c075b067c7cae789ea8848546d5
CHRISTIN JOSE BIJU <christinjb100@gmail.com>
2023-05-13 12:33:29 +0530

    create index html

--- cart/cart.js
+++ cart/cart.js
@@ -2,26 +2,26 @@ const product = [
     {
         id: 0,
         image: 'image/gg-1.jpg',
-        title: 'Z Flip Foldable Mobile',
-        price: 120,
+        title: 'Ben 10 Watch',
+        price: 650,
     },
     {
         id: 1,
         image: 'image/hh-2.jpg',
-        title: 'Air Pods Pro',
-        price: 60,
+        title: 'G Shock Watch',
+        price: 1500,
     },
     {
         id: 2,
         image: 'image/ee-3.jpg',
-        title: '250D DSLR Camera',
-        price: 230,
+        title: 'Casio Vintage Watch',
+        price: 2000,
     },
     {
         id: 3,
         image: 'image/aa-1.jpg',
-        title: 'Head Phones',
-        price: 100,
+        title: 'RADO',
+        price: 2500,
     }
 ];
 const categories = [...new Set(product.map((item)=>
